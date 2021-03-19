1963bcfcd44d0e1c793980804631032158fd6d33
Peter Yung <yungpet@gmail.com>
2021-03-19 14:55:01 -0600

    modifying environment variable to test using DATABASE_URI and postgresql:/// as per the Heroku dashboard

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.secret_key = 'jose'
